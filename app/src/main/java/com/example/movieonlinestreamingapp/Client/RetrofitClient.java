48a143a5f0368751a9528b0be0acb6b1c771473c
nhd2126 <nhohungdinh@gmail.com>
2021-06-03 11:20:37 +0700

    Comment feature done

--- app/src/main/java/com/example/movieonlinestreamingapp/Client/RetrofitClient.java
+++ app/src/main/java/com/example/movieonlinestreamingapp/Client/RetrofitClient.java
@@ -5,7 +5,7 @@ import retrofit2.Retrofit.Builder;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
-    private static final String baseUrl = "http://192.168.1.5:8081/";
+    private static final String baseUrl = "http://192.168.0.105:8081/";
     private static Retrofit retrofit = null;
 
     public static Retrofit getClient() {
